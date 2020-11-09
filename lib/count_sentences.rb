953b7c688336ea21a28c5f5dedce869c5cd63ce5
cmarker <cmarker4@gmail.com>
2020-11-09 07:42:58 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1].strip == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
+    sentences = self.split(/[.?!]+/)
+    return sentences.count
 
+    # sentences = 0
+    # arr = []
+    # self.each do |i|
+    #   arr << i
+
+    # end
+    # sentences
   end
 end
